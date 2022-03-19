7e4b14aee15277ba8c2d70e16c778b75f7b1efd1
Pedro <priccis@users.noreply.github.com>
2022-03-19 18:51:22 -0300

    feat(pwa): initial pwa setup
    - Minimal service worker
    - Manifest with share_target
    - Icons

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import { App } from "./App";
 
 const app = document.getElementById("app");
 ReactDOM.render(<App />, app);
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register(new URL("service-worker.js", import.meta.url), { type: "module" })
+    .then(function (reg) {
+      console.log("Service worker registered.");
+    })
+    .catch(function (err) {
+      console.log("Service worker not registered. This happened:", err);
+    });
+}
